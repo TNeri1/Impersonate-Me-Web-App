a283480c33f28c2cc85d706bb2f966754b342b25
Jordan Huffaker <jhuffaker@byui.edu>
2021-10-09 22:08:41 -0600

    Display images

--- Impersonate_Me_Web_App/settings.py
+++ Impersonate_Me_Web_App/settings.py
@@ -20,6 +20,12 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
