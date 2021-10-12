bebc5cb23d64acdb5a5ce33d85a5334e04cdc654
Jordan Huffaker <jhuffaker@byui.edu>
2021-10-11 18:30:22 -0600

    Create link to video page

--- main/urls.py
+++ main/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('video_upload/', views.video_upload, name='video_upload'),
     path('base/', views.base, name='base'),
-    path('show_video', views.show_video, name="show_video"),
+    path('showvideo/', views.showvideo, name="showvideo"),
 ]
