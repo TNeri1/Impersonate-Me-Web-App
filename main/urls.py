b78ce946e528d7ea6b872829b4d7aefd129f9193
Jordan Huffaker <jhuffaker@byui.edu>
2021-10-11 22:03:30 -0600

    Web app is DONE

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('index/', views.index, name='index'),
     path('video_upload/', views.video_upload, name='video_upload'),
     path('upload/', views.upload, name='upload'),
     path('showvideo/', views.showvideo, name="showvideo"),
